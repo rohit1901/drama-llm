8382397fabe772b05016be5ea0d105072f576a93
Rohit Khanduri <rohit.khanduri@hotmail.com>
2024-07-11 18:47:39 +0200

    fix: updated states

--- src/components/custom/Chat.tsx
+++ src/components/custom/Chat.tsx
@@ -9,54 +9,34 @@ import {
 } from "@/components/ui/tooltip";
 import { Button } from "@/components/ui/button";
 import { Mic, Paperclip } from "lucide-react";
-import { ChangeEvent, useEffect, useState } from "react";
-import { isArrayEmpty, isFieldEmpty } from "@/lib/utils";
+import { ChangeEvent, useState } from "react";
+import { isFieldEmpty } from "@/lib/utils";
 import { LoadingButton } from "@/components/custom/Loader";
 import { useChatStore } from "@/store/chatStore";
-import ollama from "ollama";
 
 export const Chat = () => {
   const [message, setMessage] = useState("");
-  const [loading, setLoading] = useState(false);
   const chatStore = useChatStore();
+
   const handleTextareaChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
     setMessage(e.target.value);
   };
-  useEffect(() => {
-    if (isArrayEmpty(chatStore.questions)) return;
-    setLoading(true);
-    ollama
-      .chat({
-        model: "llama3",
-        messages: chatStore.questions.map((question) => ({
-          role: "user",
-          content: question,
-        })),
-      })
-      .then((response) => {
-        chatStore.addAnswer(response.message.content);
-      })
-      .catch((error) => {
-        console.error(error);
-      })
-      .finally(() => setLoading(false));
-  }, [chatStore.questions]);
+
   return (
     <div className="relative flex h-full min-h-[50vh] flex-col rounded-xl bg-muted/50 p-4 lg:col-span-2">
-      {!isArrayEmpty(chatStore.questions) &&
-        chatStore.questions.map((question, index) => (
-          <div key={index} className="flex flex-col items-start">
-            <Badge className="mb-2">You</Badge>
-            <p className="p-3 bg-neutral rounded-lg shadow-sm">{question}</p>
-          </div>
-        ))}
-      {!isArrayEmpty(chatStore.answers) &&
-        chatStore.answers.map((answer, index) => (
-          <div key={index} className="flex flex-col items-end">
-            <Badge className="mb-2">AI</Badge>
-            <p className="p-3 bg-background rounded-lg shadow-sm">{answer}</p>
-          </div>
-        ))}
+      {chatStore.messages.map((message, index) => (
+        <div
+          key={index}
+          className={`flex flex-col items-${message.type === "question" ? "start" : "end"}`}
+        >
+          <Badge className="mb-2">
+            {message.type === "question" ? "You" : "AI"}
+          </Badge>
+          <p className="p-3 bg-neutral rounded-lg shadow-sm">
+            {message.content}
+          </p>
+        </div>
+      ))}
       <div className="flex-1" />
       <div
         className="relative overflow-hidden rounded-lg border bg-background focus-within:ring-1 focus-within:ring-ring"
@@ -96,9 +76,15 @@ export const Chat = () => {
             </Tooltip>
           </TooltipProvider>
           <LoadingButton
-            loading={loading}
-            disabled={isFieldEmpty(message) || loading}
-            onClick={() => chatStore.addQuestion(message)}
+            loading={chatStore.loading}
+            disabled={isFieldEmpty(message) || chatStore.loading}
+            onClick={() => {
+              chatStore.addMessage({
+                type: "question",
+                content: message,
+              });
+                setMessage("");
+            }}
           />
         </div>
       </div>
--- src/store/chatStore.ts
+++ src/store/chatStore.ts
@@ -1,16 +1,49 @@
 import { create } from "zustand";
+import ollama from "ollama";
 
-type ChatStore = {
-  questions: string[];
-  answers: string[];
-  addQuestion: (question: string) => void;
-  addAnswer: (answer: string) => void;
+type Message = {
+  type: "question" | "answer";
+  content: string;
 };
-export const useChatStore = create<ChatStore>((set) => ({
-  questions: [],
-  answers: [],
-  addQuestion: (question) =>
-    set((state) => ({ questions: [...state.questions, question] })),
-  addAnswer: (answer) =>
-    set((state) => ({ answers: [...state.answers, answer] })),
+type ChatStore = {
+    loading: boolean;
+    setLoading: (loading: boolean) => void;
+  messages: Message[];
+    addMessage: (message: Message) => void;
+};
+export const useChatStore = create<ChatStore>((set, get) => ({
+    loading: false,
+    messages: [],
+    setLoading: (loading) => {
+        set({ loading });
+    },
+    addMessage: async (message) => {
+        set({ loading: true });
+        // Add the message to the store
+        set({
+            messages: [...get().messages, message],
+        });
+        // If the message is a question, send it to the chatbot
+        if (message.type === "question") {
+            const response = await ollama
+                .chat({
+                    model: "llama3",
+                    messages: get().messages.filter(message => message.type === "question")?.map((question) => ({
+                        role: "user",
+                        content: question.content,
+                    })),
+                })
+            // Add the response to the store after the chatbot responds
+            set({
+                messages: [
+                    ...get().messages,
+                    {
+                        type: "answer",
+                        content: response.message.content,
+                    },
+                ],
+            });
+            set({ loading: false });
+        }
+    },
 }));
